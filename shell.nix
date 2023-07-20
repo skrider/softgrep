5fc11d3f682420e09ef8c395a695ab7de91742ea
skrider <skrider@berkeley.edu>
2023-07-20 13:16:02 -0700

    upgrade kops to latest

--- shell.nix
+++ shell.nix
@@ -1,42 +1,40 @@
 { }:
 let
     pkgs = import ../../nixpkgs {};
+    mkKops = pkgs.mkKops;
+    kopsPatch = mkKops rec {
+        version = "1.26.4";
+        sha256 = "1jfihw41cjydfq27qybdvwns7ip5n9yrgfpqwjph0jfqia91lz3l";
+        rev = "v${version}";
+    };
 in
 pkgs.mkShell {
-  buildInputs = with pkgs; [
-    # basic
-    gnumake
-    git
-    which
-    # languages
-    go
-    protobuf
-    python310
-    python3Packages.pip
-    # do stuff with files that are in the git tree
-    fd
-    # manipulate json
-    jq
-    # manipulate yaml for kubernetes
-    yq-go
-    # hydrate yaml files for k8s deploy - not the gnu one
-    envsubst
-    # test gRPC services
-    grpcurl
-    # deployment
-    docker
-    minikube
-    kubernetes-helm
-    k9s
-    kubectl
-    eksctl
-    awscli2
-    kops
-  ];
-  shellHook = ''
+    buildInputs = with pkgs; [
+        gnumake
+        git
+        which
+        go
+        protobuf
+        python310
+        python3Packages.pip
+        fd
+        jq
+        yq-go
+        envsubst
+        grpcurl
+        docker
+        minikube
+        kubernetes-helm
+        k9s
+        kubectl
+        eksctl
+        awscli2
+        kopsPatch
+    ];
+    shellHook = ''
 echo nixpgks version: ${pkgs.lib.version}
 export NIX_LD_LIB=${pkgs.python310.stdenv.cc.cc.lib}/lib
 export AWS_DEFAULT_REGION=us-west-2
 export PATH="$PATH:$(pwd)/scripts"
-  '';
+    '';
 }
